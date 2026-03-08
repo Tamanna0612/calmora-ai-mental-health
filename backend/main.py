aa3c7507acf807f7c88256d01f62aa74d93d3feb
Tamanna Shaikh <tamannashaikh0605@gmail.com>
2026-03-08 16:21:14 +0530

    Fix CORS for local frontend testing

--- backend/main.py
+++ backend/main.py
@@ -42,7 +42,9 @@ app = FastAPI(
 # ==========================================
 origins = [
     "http://localhost:3000",   # React
-    "http://localhost:5173",   
+    "http://localhost:5173", 
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",  
     "https://calmora-ai.netlify.app"
 ]
 
