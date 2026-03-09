310d27ea6d7b9582236d8165b010a9668bf1a461
Tamanna Shaikh <tamannashaikh0605@gmail.com>
2026-03-09 21:52:31 +0530

    fix CORS issue

--- backend/auth.py
+++ backend/auth.py
@@ -26,12 +26,15 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
+#def hash_password(password: str) -> str:
+ #   return pwd_context.hash(password)
 def hash_password(password: str) -> str:
+    password = password[:72]  # bcrypt limit
     return pwd_context.hash(password)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
 
 def create_access_token(data: dict) -> str:
